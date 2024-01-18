1a73b9042a0218e1acdb1fb487c438a0faa5c0b0
Ron Account <ronaccount@rons-imac.home>
2024-01-18 19:53:17 +0000

    fifth commit

--- src/data/wordData.js
+++ src/data/wordData.js
@@ -8,25 +8,6 @@ export const wordData = [
   { firstWord: 'Sugar', secondWord: 'Glucose' }
 ];
 
-// export let firstWordList = [];
-// export let secondWordList = [];
-
-// for (let i = 0; i < wordData.length; i++) {
-//     const item = wordData[i];
-  
-//     if (item.firstWord) {
-//       firstWordList.push(item.firstWord);
-//     }
-  
-//     if (item.secondWord) {
-//       secondWordList.push(item.secondWord);
-//     }
-//   }
-
-
-//   firstWordList.sort();
-//   secondWordList.sort();
-  
 export let firstWordList = wordData.map(({ firstWord }) => firstWord);
 export let secondWordList = wordData.map(({ secondWord }) => secondWord);
 
